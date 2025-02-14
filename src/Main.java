9cb5b6f4997734eaa63242580af4d076557c16f6
aladin-97 <fadhlialadin8@gmail.com>
2025-02-14 20:19:08 +0100

    add

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.printf("%d ", i);
 
+        // Création de deux instances de CompteBancaire
+        Compte compte1 = new Compte("Ali", 2015, 1000);
+        Compte compte2 = new Compte("Becher", 2018, 500);
 
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Dépôt de 500 dinars sur le premier compte
+        compte1.depot(500);
 
+        // Dépôt de 1000 dinars sur le second compte
+        compte2.depot(1000);
 
-        }
+        // Retrait de 10 dinars sur le second compte
+        compte2.retrait(10);
+
+        // Virement de 75 dinars du premier compte vers le second
+        compte1.virement(compte2, 75);
+
+        // Affichage des détails des comptes
+        System.out.println("Détails du compte 1:");
+        compte1.afficher();
+        System.out.println("\nDétails du compte 2:");
+        compte2.afficher();
+
+        // Création d'un tableau de 3 comptes
+        Compte[] comptes = new Compte[3];
+        comptes[0] = compte1;
+        comptes[1] = compte2;
+        comptes[2] = new Compte("Sarra", 2020, 2000);
+
+        // Affichage du nombre total de comptes
+        System.out.println("\nNombre total de comptes: " + Compte.getNbComptes());
+
+        // Affichage du titulaire avec le solde le plus élevé
+        System.out.println("Titulaire avec le solde le plus élevé: " + Compte.soldeMax(comptes));
     }
+
 }
